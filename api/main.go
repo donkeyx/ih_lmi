2bb180f6a6aa0e333c4eefc326562eb61a962ac7
David Binney <david@sine.com>
2021-01-05 21:30:06 +1030

    added a get single request

--- api/main.go
+++ api/main.go
@@ -10,14 +10,12 @@ import (
 )
 
 type Article struct {
+	Id      string `json:"Id"`
 	Title   string `json:"Title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
 }
 
-// let's declare a global Articles array
-// that we can then populate in our main function
-// to simulate a database
 var Articles []Article
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -30,22 +28,34 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Articles)
 }
 
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	// Loop over all of our Articles
+	// if the article.Id equals the key we pass in
+	// return the article encoded as JSON
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
 func handleRequests() {
-	// creates a new instance of a mux router
 	myRouter := mux.NewRouter().StrictSlash(true)
-	// replace http.HandleFunc with myRouter.HandleFunc
+
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/all", returnAllArticles)
-	// finally, instead of passing in nil, we want
-	// to pass in our newly created router as the second
-	// argument
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
 	Articles = []Article{
-		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
 
 	handleRequests()
